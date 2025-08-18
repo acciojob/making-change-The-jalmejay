b3024109b063a8b3ce44941ef5df175c46a4263b
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-08-19 00:06:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 const makeChange = (c) => {
+	let obj={
+		0:["q",0.25],
+		1:["d",0.10],
+		2:["n",0.05],
+		3:["p",0.01],
+	}
   // your name here
-};
+	
+	class makeUSDChange{
+		constructor(num){
+			this.money=num;
+			this.object=obj;
+		}
 
-// Do not the change the code below
+		change(){
+			let mon=this.money;
+			let count={
+		"q":0;
+		"d":0;
+		"n":0;
+		"p":0;
+	                    }
+
+			for(let i=0;i<this.object.length;i++){
+				let [symbol,value]=this.object[i];
+				while(num>value){
+					if(count.has(symbol)){
+						count.symbol++;
+					}
+					num=num-value;
+				}
+			}
+
+			return count;
+		}
+	}
+	const changes=new makeUSDChange(c)
+	changes.change();
+};
+console.log(makeChange(47))
+Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
